dc5419e26dfe54bbc8d575d0c087f28e5b9a2e22
anthonysilva98 <anthonyssilva98@gmail.com>
2021-12-06 10:43:20 -0800

    Implement Props to display text/image

--- DLingo/App.js
+++ DLingo/App.js
@@ -1,17 +1,29 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import styles from "./app.styles";
 import ImageOption from "./src/Components/ImageOptions/ImageOption";
+import question from "./data/oneQuestionWithOption";
 const App = () => {
   return (
     <View style={styles.root}>
       <Text style={styles.title}>Which of these is the "glass"?</Text>
       <View style={styles.optionsContainer}>
-        <ImageOption />
-        <ImageOption />
-        <ImageOption />
-        <ImageOption />
+        <ImageOption
+          image={question.options[0].image}
+          text={question.options[0].text}
+        />
+        <ImageOption
+          image={question.options[1].image}
+          text={question.options[1].text}
+        />
+        <ImageOption
+          image={question.options[2].image}
+          text={question.options[2].text}
+        />
+        <ImageOption
+          image={question.options[3].image}
+          text={question.options[3].text}
+        />
       </View>
     </View>
   );
