ffe80d1f976e65c40b58a9a0a92a787ae8da39f4
anthonysilva98 <anthonyssilva98@gmail.com>
2021-12-06 09:46:15 -0800

    src file with index and components w/ styles

---
+++ DLingo/src/Components/ImageOptions/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  optionContainer: {
+    borderWidth: 2,
+    borderBottomWidth: 4,
+    borderColor: "lightgrey",
+    borderRadius: 10,
+    //size
+    width: "48%",
+    height: "48%",
+    alignItems: "center",
+  },
+  optionImage: {
+    width: "48%",
+    height: "48%",
+  },
+  optionText: {},
+});
+
+export default styles;
