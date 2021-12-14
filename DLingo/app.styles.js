a95c357bf909977ca95c37afdb00468c16783d8c
anthonysilva98 <anthonyssilva98@gmail.com>
2021-12-14 15:43:57 -0800

    Create New Files, split logic into components

--- DLingo/app.styles.js
+++ DLingo/app.styles.js
@@ -8,19 +8,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingTop: 30,
   },
-  title: {
-    fontSize: 25,
-    fontWeight: "bold",
-    alignSelf: "stretch",
-  },
-  optionsContainer: {
-    width: "100%",
-    flexDirection: "row",
-    flex: 1,
-    flexWrap: "wrap",
-    justifyContent: "space-between",
-    alignContent: "space-between",
-  },
 });
 
 export default styles;
